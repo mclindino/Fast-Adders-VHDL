8c53198c30303b6e4c829ee9ad02c109124f0f13
Lindino <matlindino00@gmail.com>
2019-09-30 16:40:47 -0300

    Update

---
+++ LOOKAHEAD/RTL Compiler/Cenario_0/rc.cmd
@@ -0,0 +1,9 @@
+# Cadence Encounter(R) RTL Compiler
+#   version RC11.10 - v11.10-p005_1 (64-bit) built Nov 10 2011
+#
+# Run with the following arguments:
+#   -files ../scripts/script.tcl
+#   -logfile rc.log
+#   -cmdfile rc.cmd
+
+source ../scripts/script.tcl
